2d8ce3fb594f166cff2c4ae70f802f481a2b1c84
Johannes Ewald <johannes.ewald@peerigon.com>
2015-03-13 19:19:33 +0100

    Add tests for custom importers

--- test/prepare.js
+++ test/prepare.js
@@ -6,21 +6,42 @@ var path = require('path');
 
 ['scss', 'sass'].forEach(function (ext) {
     var files = [];
+    var basePath = path.join(__dirname, ext);
 
     fs.readdirSync(path.join(__dirname, ext))
         .filter(function (file) {
             return path.extname(file) === '.' + ext;
         })
         .map(function (file) {
-            var filename;
+            var fileName = path.join(basePath, file);
+            var fileContent;
+            var css;
 
-            files.push(filename = path.join(__dirname, ext, file));
-            return sass.renderSync({
-                file: filename,
+            if (file === 'imports.' + ext) {
+                // We need to replace all occurrences of '~' with relative paths
+                // so node-sass finds the imported files without webpack's resolving algorithm
+                fileContent = fs.readFileSync(fileName, 'utf8');
+                fileContent = fileContent.replace('~', '../node_modules/');
+                fs.writeFileSync(fileName, fileContent, 'utf8');
+            }
+
+            files.push(fileName);
+
+            css = sass.renderSync({
+                file: fileName,
                 includePaths: [
                     path.join(__dirname, ext, 'another')
                 ]
             }).css;
+
+            if (file === 'imports.' + ext) {
+                // Revert back our changes and add '~' again
+                fileContent = fs.readFileSync(fileName, 'utf8');
+                fileContent = fileContent.replace('../node_modules/', '~');
+                fs.writeFileSync(fileName, fileContent, 'utf8');
+            }
+
+            return css;
         })
         .forEach(function (content, index) {
             fs.writeFileSync(files[index].replace(new RegExp('\\.' + ext + '$', 'gi'), '.css'), content, 'utf8');
