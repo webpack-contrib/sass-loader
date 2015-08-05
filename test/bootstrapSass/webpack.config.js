c5f311f2614723129d1d1acbda8d7b136c3badda
Johannes Ewald <johannes.ewald@peerigon.com>
2015-08-05 17:05:47 +0200

    Add twitter bootstrap test

---
+++ test/bootstrapSass/webpack.config.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var path = require('path');
+
+var pathToSassLoader = path.resolve(__dirname, '../../index.js');
+
+module.exports = {
+    entry: path.resolve(__dirname, '../scss/bootstrap-sass.scss'),
+    output: {
+        path: path.resolve(__dirname, '../output'),
+        filename: 'bundle.bootstrap-sass.js'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.woff2?$|\.ttf$|\.eot$|\.svg$/,
+                loader: 'file'
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!' + pathToSassLoader
+            }
+        ]
+    }
+};
