webpack-contrib/sass-loader

commit fd6be1f143a1810f7e5072a865b3d8675ba1b94e

Use resoucePath for custom importers
Fixes #234

index.js
@@ -245,7 +245,13 @@ module.exports = function (content) {
     }
 
     // Allow passing custom importers to `node-sass`. Accepts `Function` or an array of `Function`s.
-    sassOptions.importer = sassOptions.importer ? [].concat(sassOptions.importer) : [];
+    sassOptions.importer = sassOptions.importer ? [].concat(sassOptions.importer).map(function(importer) {
+            return function(url, prev, done) {
+                return importer(url, prev === 'stdin' ? resourcePath : prev, done);
+            };
+        })
+        : []
+    ;
     sassOptions.importer.push(getWebpackImporter());
 
     // `node-sass` uses `includePaths` to resolve `@import` paths. Append the currently processed file.

test/tools/customImporter.js
@@ -2,8 +2,9 @@
 
 var should = require('should');
 
-function customImporter(path) {
+function customImporter(path, prev) {
     path.should.equal('import-with-custom-logic');
+    prev.match(process.cwd() + '/test/(sass|scss)/custom-importer.(scss|sass)').should.not.equal(null);
     return customImporter.returnValue;
 }
 customImporter.returnValue = {

commit d894345fa509a10d4a3f3c8ef88a26cef4ea3366

Fix wrong context in customImporters
This is a regression that was introduced with fd6be1f143a1810f7e5072a865b3d8675ba1b94e

#275 #277

index.js
@@ -18,6 +18,7 @@ var SassError = {
 };
 
 // libsass uses this precedence when importing files without extension
+var slice = Array.prototype.slice;
 var extPrecedence = ['.scss', '.sass', '.css'];
 var matchCss = /\.css$/;
 
@@ -409,7 +410,13 @@ function getLoaderConfig(loaderContext) {
 function proxyCustomImporters(importer, resourcePath) {
     return [].concat(importer).map(function (importer) {
         return function (url, prev, done) {
-            return importer(url, prev === 'stdin' ? resourcePath : prev, done);
+            var args = slice.call(arguments);
+            
+            if (args[1] === 'stdin') {
+                args[1] = resourcePath;
+            }
+            
+            return importer.apply(this, args);
         };
     });
 }

test/tools/customImporter.js
@@ -3,8 +3,13 @@
 var should = require('should');
 
 function customImporter(path, prev) {
+    /*jshint validthis: true */
+    
     path.should.equal('import-with-custom-logic');
     prev.should.match(/(sass|scss)[/\\]custom-importer\.(scss|sass)/);
+    
+    this.should.have.property('options');
+    
     return customImporter.returnValue;
 }
 customImporter.returnValue = {