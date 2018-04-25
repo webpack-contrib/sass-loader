10a816187fcf5c1d2b2625ce2336db441816d7b6
Johannes Ewald <mail@johannesewald.de>
2018-04-25 14:08:30 +0200

    test: Add test cases to ensure that js files do not take precedence (#564)

---
+++ test/scss/another/module.js
@@ -0,0 +1,3 @@
+// ./another/module.js: The sass-loader should not try to import that. scss, sass and css extensions should be preferred.
+// See https://github.com/webpack-contrib/sass-loader/issues/556#issuecomment-381154009
+"This should not be imported";
