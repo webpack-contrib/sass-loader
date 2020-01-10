d6f514159120ce75abc9b8bcfc92fdb7adb0b803
evilebottnawi <sheo13666q@gmail.com>
2020-01-10 15:27:32 +0300

    chore(deps): update

--- test/cjs.test.js
+++ test/cjs.test.js
@@ -1,8 +1,8 @@
-import loader from '../src';
-import CJSLoader from '../src/cjs';
+import src from '../src';
+import cjs from '../src/cjs';
 
 describe('cjs', () => {
-  it('should exported loader', () => {
-    expect(CJSLoader).toEqual(loader);
+  it('should exported', () => {
+    expect(cjs).toEqual(src);
   });
 });
