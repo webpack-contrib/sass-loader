8a0bc0a86e7853d7709b708ae6bae1137ff2f936
evilebottnawi <sheo13666q@gmail.com>
2020-12-04 20:21:21 +0300

    test: more

--- test/loader.test.js
+++ test/loader.test.js
@@ -1631,6 +1631,18 @@ describe("loader", () => {
           expect(getErrors(stats)).toMatchSnapshot("errors");
         });
 
+        it(`should not use .import.${syntax} files (${implementationName}) (${syntax})`, async () => {
+          const testId = getTestId("use-index-import", syntax);
+          const options = {
+            implementation: getImplementationByName(implementationName),
+          };
+          const compiler = getCompiler(testId, { loader: { options } });
+          const stats = await compile(compiler);
+
+          expect(getWarnings(stats)).toMatchSnapshot("warnings");
+          expect(getErrors(stats)).toMatchSnapshot("errors");
+        });
+
         it(`should work and output deprecation message (${implementationName})`, async () => {
           const testId = getTestId("deprecation", syntax);
           const options = {
