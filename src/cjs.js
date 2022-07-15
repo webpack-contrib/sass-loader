676aac7f1ac75d03fc24d0e6a199f02b4e382ef6
Sachin Kumar <ersachin3112@gmail.com>
2022-07-15 14:14:49 +0530

    chore: remove cjs.js

--- src/cjs.js
+++
@@ -1,3 +0,0 @@
-const loader = require("./index");
-
-module.exports = loader;
--- test/cjs.test.js
+++
@@ -1,8 +0,0 @@
-const src = require("../src");
-const cjs = require("../src/cjs");
-
-describe("cjs", () => {
-  it("should exported", () => {
-    expect(cjs).toEqual(src);
-  });
-});
