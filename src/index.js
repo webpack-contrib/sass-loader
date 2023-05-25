4118361d277175a07437100ade884c588bbda285
alexander.akait <sheo13666q@gmail.com>
2023-05-25 23:44:53 +0300

    test: update

--- src/index.js
+++ src/index.js
@@ -32,14 +32,6 @@ async function loader(content) {
     return;
   }
 
-  if (!implementation) {
-    callback(
-      new Error(`The Sass implementation "${options.implementation}" not found`)
-    );
-
-    return;
-  }
-
   const useSourceMap =
     typeof options.sourceMap === "boolean" ? options.sourceMap : this.sourceMap;
   const sassOptions = await getSassOptions(
