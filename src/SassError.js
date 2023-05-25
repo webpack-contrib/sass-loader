1f7b9bd8746b530407b89f2def18e96de8b85b17
alexander.akait <sheo13666q@gmail.com>
2023-05-26 00:16:46 +0300

    fix: improve error message

--- src/SassError.js
+++ src/SassError.js
@@ -7,7 +7,6 @@ class SassError extends Error {
     // Instruct webpack to hide the JS stack from the console.
     // Usually you're only interested in the SASS error in this case.
     this.hideStack = true;
-    Error.captureStackTrace(this, this.constructor);
 
     if (
       typeof sassError.line !== "undefined" ||
@@ -25,10 +24,7 @@ class SassError extends Error {
     }`;
 
     if (sassError.formatted) {
-      this.message = `${this.name}: ${sassError.formatted.replace(
-        /^Error: /,
-        ""
-      )}`;
+      this.message = sassError.formatted.replace(/^Error: /, "");
     }
   }
 }
