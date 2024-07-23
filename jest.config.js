b156215353e7cfa18a266c9a0f8b51f10025a683
alexander.akait <sheo13666q@gmail.com>
2024-07-23 18:53:25 +0300

    ci: fix

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   testEnvironment: "jest-environment-node-single-context",
+  moduleNameMapper: {
+    // For Node.js@22, because `node-sass` doesn't support it
+    "^node-sass$": "sass",
+  },
 };
